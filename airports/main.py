3b4dfa5462cc47e9bad29ef9fa517949b786e148
squirrelcoding <rosilescarlos28@gmail.com>
2025-05-29 10:54:50 -0500

    fine tuned??

--- airports/main.py
+++ airports/main.py
@@ -1,33 +1,103 @@
 import os
+import json
+from typing import Any
+
 import torch
-from torchvision import transforms
-from torchvision.models.detection import fasterrcnn_resnet50_fpn
-from airport_dataset import AirportDataset
 from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
+
+from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights
+from torchvision import tv_tensors
+from torchvision.transforms.v2 import functional as F
+from torchvision.transforms.v2.functional import to_dtype
+
+import PIL.Image
 
 torch.manual_seed(42)
 
 BATCH_SIZE = 32
 NUM_WORKERS = os.cpu_count()
 
-data_transform = transforms.Compose([
-    transforms.ToTensor()
-])    
+class AirportDataset(torch.utils.data.Dataset):
+    def __init__(self, root_dir: str, annotation_file: str, transform=None):
+        self.transform = transform
+        self.data = []
+
+        with open(annotation_file) as f:
+            annotations: dict = json.load(f)
+            for entry in annotations.values():
+                filename = entry["filename"]
+                regions = entry["regions"]
+
+                boxes = []
+                for region in regions:
+                    x = region["shape_attributes"]["x"]
+                    y = region["shape_attributes"]["y"]
+                    w = region["shape_attributes"]["width"]
+                    h = region["shape_attributes"]["height"]
+                    boxes.append([x, y, x + w, y + h])
+
+                self.data.append({
+                    "img_path": os.path.join(root_dir, filename),
+                    "boxes": boxes
+                })
+
+    def __getitem__(self, idx: int) -> Any:
+        entry = self.data[idx]
+        img = PIL.Image.open(entry["img_path"]).convert("RGB")
+
+        # Resize the image to 400 x 400 pixels and calculate the scale factors
+        scale_x = 400 / img.width
+        scale_y = 400 / img.height
+        img = img.resize((400, 400))
+
+        boxes = torch.tensor(entry["boxes"], dtype=torch.float32)
+
+        # Scale the boxes
+        boxes[:, 2] *= scale_x
+        boxes[:, 0] *= scale_x
+        boxes[:, 1] *= scale_y
+        boxes[:, 3] *= scale_y
+
+        # convert the image
+        img = to_dtype(tv_tensors.Image(img), torch.float64)
+
+        target = {}
+
+        # Update boxes so that its a set of bounding boxes
+        target["boxes"] = tv_tensors.BoundingBoxes(
+            boxes, 
+            format="XYXY", 
+            canvas_size=F.get_size(img) # type: ignore
+        )
+        target["labels"] = torch.ones((boxes.shape[0],), dtype=torch.int64)
+        target["image_id"] = torch.tensor(idx)
+        target["area"] = (boxes[:, 2] - boxes[:, 0]) * (boxes[:, 3] - boxes[:, 1])
+        target["iscrowd"] = torch.zeros((boxes.shape[0],), dtype=torch.int64)
+
+        if self.transform is not None:
+            img, target = self.transform(img, target)
+
+        return img, target
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+def collate_fn(batch):
+    return tuple(zip(*batch))
 
 
-dataset = AirportDataset("data", "via_export_json.json", transform=data_transform)
+dataset = AirportDataset("data", "via_export_json.json")
 
 # Form new training and testing dataloaders
 N = len(dataset)
-
 train_dataset, test_dataset = torch.utils.data.random_split(dataset, [N - int(0.1 * N), int(0.1 * N)])
 
 train_dataloader = DataLoader(
     train_dataset, 
     batch_size=BATCH_SIZE, 
     shuffle=True, 
+    collate_fn=collate_fn,
     num_workers=NUM_WORKERS # type: ignore
 )
 
@@ -38,34 +108,11 @@ test_dataloader = DataLoader(
     num_workers=NUM_WORKERS # type: ignore
 )
 
-model = fasterrcnn_resnet50_fpn(pretrained=True)
+model = fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
 
-# 1. Get a batch of images and labels from the DataLoader
-img_batch, label_batch = next(iter(train_dataloader))
+model.train()
 
-img = img_batch[20]
+train_loss, train_acc = 0, 0
 
-# # 3. Perform a forward pass on a single image
-model.eval()
-with torch.inference_mode():
-    pred = model(img_batch[0].unsqueeze(dim=0))
-    boxes = pred[0]['boxes']
-    scores = pred[0]['scores']
-    labels = pred[0]['labels']
-
-
-fig, ax = plt.subplots(1)
-ax.imshow(img.permute(1, 2, 0))
-
-# Add boxes
-for box, score, label in zip(boxes, scores, labels):
-    x1, y1, x2, y2 = box.tolist()
-    width = x2 - x1
-    height = y2 - y1
-    rect = patches.Rectangle((x1, y1), width, height, linewidth=2, edgecolor='red', facecolor='none')
-    ax.add_patch(rect)
-    ax.text(x1, y1 - 5, f'{label.item()} ({score:.2f})', color='red', fontsize=8, backgroundcolor='white')
-
-plt.axis('off')
-plt.savefig("img.png")
-plt.show()
+for X, y in train_dataloader:
+    pred = model(X, y)
