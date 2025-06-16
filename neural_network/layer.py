829d2340d84bee51b802c4ed3ba69c0cedf58fe6
squirrelcoding <rosilescarlos28@gmail.com>
2025-06-16 06:45:54 -0500

    worked on backpropagation

--- neural_network/layer.py
+++ neural_network/layer.py
@@ -21,8 +21,12 @@ class Layer():
     """
     Base class for layers.
     """
+    def __init__(self):
+        self.activations = np.array([])
     def forward(self, input_weights: npt.NDArray):
         return input_weights
+    def backward(self, grad_output: npt.NDArray) -> npt.NDArray:
+        return grad_output
 
 class Linear(Layer):
     """
@@ -32,6 +36,9 @@ class Linear(Layer):
         # Wrap each float in a DiffScalar
         self.weights = np.array([[DiffScalar(w) for w in row] 
                                  for row in np.random.rand(out_features, in_features)], dtype=object)
+
+        self.inputs = np.array([DiffScalar(0.0) for _ in range(in_features)], dtype=object)
+        self.activations = np.array([DiffScalar(0.0) for _ in range(out_features)], dtype=object)
         if bias:
             self.bias = np.array([DiffScalar(b) 
                                   for b in np.random.rand(out_features)], dtype=object)
@@ -41,8 +48,34 @@ class Linear(Layer):
 
     def forward(self, input_weights: np.ndarray):
         # Assumes input_weights is a vector of DiffScalars (shape: [in_features])
-        r = dot(self.weights, input_weights) + self.bias
-        return r
+        self.inputs = input_weights
+        self.activations = dot(self.weights, input_weights) + self.bias
+        return self.activations
+    def backward(self, grad_output: npt.NDArray) -> npt.NDArray:
+        """
+        Given the output, this method calculates the gradient of the loss with respect to 
+        the output `∂L/∂a` and computes the gradient of the loss with respect to its inputs (`∂L/∂x`)
+        and the gradient of the loss with respect of the loss with respect to its weights and biases.
+
+        It returns `∂L/∂x` to the previous layer.
+        """
+        
+        
+        # Reset adjoints
+        for activation in self.activations:
+            activation.adjoint = 0.0
+        
+        # Loop through the current activations
+        for i in range(len(grad_output)):
+            # Call backward
+            self.activations[i].backward(grad_output[i])
+
+        activation_grad = np.array([x.adjoint for x in self.inputs], dtype=object)
+        
+        # Loop through the weights and biases
+        
+        return activation_grad
+
     def __str__(self):
         s = ""
         for weight in self.weights:
@@ -65,9 +98,10 @@ def sigmoid(x: np.ndarray):
 
 class Sigmoid(Layer):
     def __init__(self) -> None:
-        pass
+        self.activations = []
     def forward(self, input_weights: npt.NDArray):
-        return sigmoid(input_weights)
+        self.activations = sigmoid(input_weights)
+        return self.activations
 
 def relu_scalar(x: DiffScalar):
     if x.primal > 0:
@@ -88,9 +122,10 @@ def relu(x: np.ndarray):
 
 class ReLU(Layer):
     def __init__(self) -> None:
-        pass
+        self.activations = []
     def forward(self, input_weights: npt.NDArray):
-        return relu(input_weights)
+        self.activations = relu(input_weights)
+        return self.activations
 
 W = np.array([[DiffScalar(0.1), DiffScalar(0.2)],
               [DiffScalar(0.3), DiffScalar(0.4)]])
@@ -98,4 +133,10 @@ x = np.array([[DiffScalar(1.0)], [DiffScalar(2.0)]])
 
 out = dot(W, x)
 
-# print(out)
+#    X
+# O  X     O
+# O  X     O
+# O  X
+#    X
+#    X
+# in=3, out=2
--- neural_network/nn.py
+++ neural_network/nn.py
@@ -14,16 +14,18 @@ class NN():
         for layer in self.layers:
             x = layer.forward(x)
         return x
-    def calculate_gradient(self, layer: int, node: int):
+    def calculate_gradient(self, idx: int):
         """
-        Given two indices that correspond to a node in a trainable, non-input layer, 
-        calculate the partial derivative of it with respect to all of the weights and biases
-        of the previous layer.
+        Given an index that corresponds to a layer, calculate the partial derivative of it with respect to all of the weights and biases of the previous layer.
         """
-        if layer == 0 or layer >= len(self.layers):
+        if idx == 0 or idx >= len(self.layers):
             raise IndexError("Invalid layer index. Must be greater than 0 or less than len(self.layers)")
         
-        # Reset the adjoints of the weights and biases of the previous layer
+        # Loop through the nodes of the layer
+        for node in self.layers[idx].activations:
+            # Reset the adjoints of the weights and biases of the previous layer
+            self.zero_grad(idx - 1)
+            pass
         
     def zero_grad(self, layer: int):
         """
@@ -47,8 +49,32 @@ x = np.array([DiffScalar(1.0, 2.0), DiffScalar(-2.0, -3.0), DiffScalar(3.0, 0.1)
 
 x = nn.forward(x)
 
-# nn.zero_grad(0)
+nn.zero_grad(2)
 
 print(nn.layers[0])
 
 # GOAL: Calculate the derivative between a single node and all of the neurons in the layer RIGHT BEFORE IT
+# The thing that is confusing me the most right now is how to actually go about how to calculate the gradient.
+# Originally the goal was to simply call node.backward(1.0) but then I realized that in real neural networks
+# like the one being built right now, we don't acutllay have independent variables that we can call .backward()
+# on. Rather, for each layer we just have a matrix of weights and a bias vector. So what should we do if we want
+# to calculate the gradients between two layers?
+# Let's say that the previous layer has L inputs, M outputs and the current layer has M inputs and N outputs (nodes). So we have (L x M) and (M x N).
+# ((L + 1) x M) matrix representing the weights and biases of the previous layer
+# ((M + 1) x N) matrix representing the weights and biases of the current layer.
+
+# There are N nodes, and for each node there are M weights and 1 bias. Thus, the gradient matrix should be like 
+# an (M + 1) x N matrix. However, recall that in gradient descent we just take the average of all of the column
+# entries to end up with an (M + 1) x 1 gradient vector that we use to adjust everything
+
+# So for each of the N activations in the current layer, ask: how sensitive is this node with respect to all of
+# the nodes in the previous layer?
+
+# BUT HOW DO WE ACTUALLY DO THIS? 
+# One way that we *could* possibly do it is to manually calculate the activation of the new node and then
+# call the .backward(1.0) method on it. However, this feels like a waste of memory, when, in a sense, we already
+# have the node with us.
+
+# DOING THE BACKPROPAGATION ONLY MAKES SENSE WHEN WE ALREADY HAVE ACTIVATIONS.
+# Okay, so all of the layers now have an activation field.
+# 
